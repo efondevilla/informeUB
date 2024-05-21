e9dc42960babd37b72549a9e9a2566355c849b10
Edgar Fondevilla <efondevilla@sirt.com>
2024-05-21 17:37:47 +0200

    Generate Microsoft JWT fetching funcion

--- src/data/microsoft/index.ts
+++ src/data/microsoft/index.ts
@@ -1,29 +1,27 @@
 import { env } from 'node:process'
 
-const { MICROSOFT_CLIENT_ID, MICROSOFT_OBJECT_ID, MICROSOFT_TENANT_ID, MICROSOFT_SECRET_ID, MICROSOFT_SECRET_VALUE_ID } = env
-// const API_CENTRAL_URL = 'https://eu.api.security.microsoft.com'
+const { MICROSOFT_CLIENT_ID, MICROSOFT_TENANT_ID, MICROSOFT_SECRET_VALUE_ID } = env
 let JWTtoken: string | undefined = undefined
-
 export async function getMicrosoftJWT(force: boolean = false) {
   if (JWTtoken && !force)
     return JWTtoken
   if (!MICROSOFT_CLIENT_ID)
-    throw new Error("Missing Microsoft Client client ID")
-  if (!MICROSOFT_SECRET_ID)
-    throw new Error("Missing Microsoft Client secret ID")
+    throw new Error("Missing Microsoft Client ID")
+  if (!MICROSOFT_SECRET_VALUE_ID)
+    throw new Error("Missing Microsoft Secret ID")
   if (!MICROSOFT_TENANT_ID)
-    throw new Error("Missing Microsoft Client tenant ID")
+    throw new Error("Missing Microsoft Tenant ID")
 
   const myHeaders = new Headers({"Content-Type": "application/x-www-form-urlencoded"});
   const urlencoded = new URLSearchParams({
     grant_type: 'client_credentials',
     client_id: MICROSOFT_CLIENT_ID,
-    client_secret: MICROSOFT_SECRET_ID,
-    scope: `https://graph.microsoft.com/.default`,
+    client_secret: MICROSOFT_SECRET_VALUE_ID,
+    scope: 'https://api.securitycenter.microsoft.com/.default',
   })
 
   try{
-    const response = await fetch("https://login.microsoftonline.com/" + MICROSOFT_TENANT_ID + "/oauth2/v2.0/token", {
+    const response = await fetch(`https://login.microsoftonline.com/${MICROSOFT_TENANT_ID}/oauth2/v2.0/token`, {
       method: "POST",
       headers: myHeaders,
       body: urlencoded,
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,10 @@
-import { getSophosAlerts, getSophosEvents } from 'data/sophos/sophosSiem'
+import { getMicrosoftJWT } from 'data/microsoft'
+// import { getSophosAlerts, getSophosEvents } from 'data/sophos/sophosSiem'
 
 // const alerts = await getSophosAlerts()
 // const events = await getSophosEvents()
-
 // console.log('alerts: ',alerts.length)
 // console.log('events: ',events.length)
+
+const token = await getMicrosoftJWT()
+console.log(token)
